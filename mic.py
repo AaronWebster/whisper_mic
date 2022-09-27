91c46f24f84a91c12bffbce777ce0e08b43d507f
awexander <shafiqyaacob11@gmail.com>
2022-09-27 15:46:53 +0800

    intial commit

--- mic.py
+++ mic.py
@@ -1,11 +1,13 @@
 import io
 from pydub import AudioSegment
 import speech_recognition as sr
+import pyaudio
+import wave
 import whisper
 import tempfile
 import os
 import click
-
+from datetime import datetime
 
 temp_dir = tempfile.mkdtemp()
 save_path = os.path.join(temp_dir, "temp.wav")
@@ -18,6 +20,7 @@ save_path = os.path.join(temp_dir, "temp.wav")
 @click.option("--energy", default=300, help="Energy level for mic to detect", type=int)
 @click.option("--dynamic_energy", default=False,is_flag=True, help="Flag to enable dynamic engergy", type=bool)
 @click.option("--pause", default=0.8, help="Pause time before entry ends", type=float)
+
 def main(model, english,verbose, energy, pause,dynamic_energy):
     #there are no english models for large
     if model != "large" and english:
@@ -28,22 +31,72 @@ def main(model, english,verbose, energy, pause,dynamic_energy):
     r = sr.Recognizer()
     r.energy_threshold = energy
     r.pause_threshold = pause
-    r.dynamic_energy_threshold = dynamic_energy
+    r.dynamic_energy_threshold = False
 
-    with sr.Microphone(sample_rate=16000) as source:
-        print("Say something!")
-        while True:
-            #get and save audio to wav file
-            audio = r.listen(source)
-            data = io.BytesIO(audio.get_wav_data())
-            audio_clip = AudioSegment.from_file(data)
-            audio_clip.export(save_path, format="wav")
+    RATE = 44100
+    CHANNELS = 2
+    DEVICE_INDEX = 11
+    FORMAT = pyaudio.paInt16
+    FRAMESBUFF = 1024
+    SECONDS = 5
 
-            result = audio_model.transcribe(save_path)
-            if not verbose:
-                predicted_text = result["text"]
-                print("You said: " + predicted_text)
-            else:
-                print(result)
+    daudio = pyaudio.PyAudio()
+    for i in range(0, daudio.get_device_count()):
+        print(daudio.get_device_info_by_index(i))
+    audiostream = daudio.open(output_device_index=DEVICE_INDEX,
+                                rate=RATE, 
+                                input=True, 
+                                channels=CHANNELS,
+                                format=FORMAT,
+                                frames_per_buffer=FRAMESBUFF)
+    print("Say something!")
+    index = 0
+    while True:
+        #get and save audio to wav file
+        prevTime = datetime.now()
+        #try:
+        print('listening...')
+        frame = []
+        audiostream.start_stream()
+        for i in range(0, int(RATE / FRAMESBUFF * SECONDS)):
+            data = audiostream.read(FRAMESBUFF)
+            frame.append(data)
+
+        audiostream.stop_stream()
+        print(datetime.now() - prevTime)
+        print('saving audio file')
+        audioPath = f"audio/audio{index}.wav"
+        with wave.open(audioPath, 'wb') as w:
+            w.setnchannels(2)
+            w.setsampwidth(daudio.get_sample_size(FORMAT))
+            w.setframerate(RATE)
+            w.writeframes(b''.join(frame))
+            w.close()
+        print('transcribing...')
+        result = audio_model.transcribe(audioPath)
+        if not verbose:
+            predicted_text = result["text"]
+            print("You said: " + predicted_text)
+        else:
+            print(result)
+        index += 1
                 
 main()
+"""
+                audio = r.listen(source, timeout=5)
+                print(datetime.now() - prevTime)
+                print('converting...')
+                data = io.BytesIO(audio.get_wav_data())
+                print('saving audio...')
+                audio_clip = AudioSegment.from_file(data)
+                audio_clip.export(save_path, format="wav")
+                
+                time = datetime.now()
+                print('transcribing...')
+                result = audio_model.transcribe(save_path)
+                print(datetime.now() - time)
+                if not verbose:
+                    predicted_text = result["text"]
+                    print("You said: " + predicted_text)
+                else:
+                    print(result)"""
